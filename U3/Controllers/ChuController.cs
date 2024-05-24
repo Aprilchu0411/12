2f0e9f2219367856a375bb5c813197567595a269
esico <esico@LAPTOP-HQGPH4SB>
2024-05-24 10:59:13 +0800

    加入專案檔案。

---
+++ U3/Controllers/ChuController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace U3.Controllers
+{
+    public class ChuController : Controller
+    {
+        public IActionResult A()
+        {
+            return View();
+        }
+        public IActionResult B()
+        {
+            return View();
+        }
+        public IActionResult C()
+        {
+            return View();
+        }
+        public IActionResult D()
+        {
+            return View();
+        }
+    }
+}
---
+++ U3/Controllers/JaneController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace U3.Controllers
+{
+    public class JaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult A1()
+        {
+            return View();
+        }
+        public IActionResult A2()
+        {
+            return View();
+        }
+        public IActionResult A3()
+        {
+            return View();
+        }
+    }
+}
